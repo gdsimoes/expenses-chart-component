0a26f955b8c62ea45f8dcfe5b4f5b8284d23f24a
Guilherme Dias Simoes <gds.simoes@gmail.com>
2022-07-19 21:53:34 -0400

    Fix bar heights bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+"use strict";
+
+const cyan = "#76B5BC";
+
 async function getData(url) {
     const requestURL = url;
     const request = new Request(requestURL);
@@ -10,13 +14,18 @@ async function getData(url) {
 
 function populateBars(jsonArr) {
     const maxAmount = jsonArr.reduce(
-        (max, obj) => (max < obj.amount ? obj.amount : max),
+        (max, obj) =>
+            Number(max) < Number(obj.amount) ? Number(obj.amount) : Number(max),
         0
     );
 
     for (const obj of jsonArr) {
         const bar = document.querySelector(`#${obj.day}`);
-        bar.style.height = `${(150 / 177) * (obj.amount / maxAmount) * 100}%`;
+        bar.style.height = `${(150 / 375) * (obj.amount / maxAmount) * 100}vw`;
+        bar.style.transitionDuration = `${(obj.amount / maxAmount) * 2}s`;
+        if (obj.amount === maxAmount) {
+            bar.style.backgroundColor = cyan;
+        }
     }
 }
 
