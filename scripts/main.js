e7d5dac7ae1550067fbf4515c8d700ff6c34c6e3
Guilherme Dias Simoes <gds.simoes@gmail.com>
2022-07-21 22:45:04 -0400

    Finish basic layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Draw graph
 const cyan = "#76B5BC";
 
 async function getData(url) {
@@ -19,14 +20,42 @@ function populateBars(jsonArr) {
         0
     );
 
-    for (const obj of jsonArr) {
-        const bar = document.querySelector(`#${obj.day}`);
-        bar.style.height = `${(150 / 375) * (obj.amount / maxAmount) * 100}vw`;
-        bar.style.transitionDuration = `${(obj.amount / maxAmount) * 2}s`;
-        if (obj.amount === maxAmount) {
-            bar.style.backgroundColor = cyan;
+    if (window.innerWidth >= 700) {
+        for (const obj of jsonArr) {
+            const bar = document.querySelector(`#${obj.day}`);
+            bar.style.height = `${(obj.amount / maxAmount) * 150}px`;
+            bar.style.transitionDuration = `${(obj.amount / maxAmount) * 2}s`;
+            if (obj.amount === maxAmount) {
+                bar.style.backgroundColor = cyan;
+            }
+        }
+    } else {
+        for (const obj of jsonArr) {
+            const bar = document.querySelector(`#${obj.day}`);
+
+            bar.style.height = `${
+                (150 / 375) * (obj.amount / maxAmount) * 100
+            }vw`;
+            bar.style.transitionDuration = `${(obj.amount / maxAmount) * 2}s`;
+            if (obj.amount === maxAmount) {
+                bar.style.backgroundColor = cyan;
+            }
         }
     }
 }
 
 getData("data.json");
+
+// Scale the design for bigger screens
+const scalar = () => {
+    const container = document.querySelector(".container");
+    if (window.innerWidth >= 700) {
+        container.style.transform = `scale(${window.innerWidth / 1440})`;
+    } else {
+        container.style.transform = `scale(1)`;
+    }
+};
+
+window.onresize = scalar;
+
+scalar();
